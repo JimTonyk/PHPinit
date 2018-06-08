89dd3297798379e2bd13d32769a5b54e05454156
JM. Hiltbrunner <jm.hiltbrunner@gmail.com>
2018-06-08 17:22:28 +0200

    Gestion des exceptions métiers par une page erreur dédiée

--- index.php
+++ index.php
@@ -49,5 +49,6 @@ try{
     }
 }
 catch(Exception $e){
-    echo 'Erreur : '.$e -> getMessage();
+    $errorMessage = $e -> getMessage();
+    require('\view\frontend\errorView.php');
 }
---
+++ view/frontend/errorView.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+<html>
+<!-- If any error is catch during process, display this error in an dadicated page -->
+<?php $title='Erreur rencontrée';
+      ob_start(); ?>
+<body>   
+<h1>Une erreur a été rencontrée lors de l'éxecution de la requête</h1>
+    <p><?= $errorMessage ?></p>
+    <a href="index.php">Retour à la page d'accueil</a>
+    <?php $content = ob_get_clean();
+          require'template.php'; ?>
+</body>
+
+</html>
